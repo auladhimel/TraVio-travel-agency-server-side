50d225f92e920538e08fdf32cc078c01f4d54872
auladhimel <himelaulad3021@gmail.com>
2021-11-01 21:16:28 +0600

    final push

--- index.js
+++ index.js
@@ -16,11 +16,7 @@ const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology:
 
 
 
-// const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.tropq.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
-// const client = new MongoClient(uri,
-//   {
-//     useNewUrlParser: true, useUnifiedTopology: true
-//   });
+
 
 console.log(uri)
 async function run() {
