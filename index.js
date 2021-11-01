dc74253ce2b90512430ea70668e656ed891cb4dc
auladhimel <himelaulad3021@gmail.com>
2021-11-01 23:15:46 +0600

    final

--- index.js
+++ index.js
@@ -15,9 +15,6 @@ const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@clu
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 
 
-
-
-
 console.log(uri)
 async function run() {
   try {
@@ -132,23 +129,15 @@ async function run() {
         });
     });
 
-
-
-
-
   }
   finally { }
 }
 run().catch(console.dir)
 
 
-
-
-
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
-
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
